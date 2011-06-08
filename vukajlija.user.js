7326cee1eeab4ca3dc217a9ecb631c47cdaa2e52
Goran Gajic <fero.null@gmail.com>
2011-06-08 18:54:47 +0200

    changed path to script

--- vukajlija.user.js
+++ vukajlija.user.js
@@ -16,7 +16,7 @@
     (function(){
             var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
                 GM_Script = document.createElement('script');
-            GM_Script.src = 'https://raw.github.com/feroc1ty/Vukajlija-extension/master/vukajlija.js';
+            GM_Script.src = 'https://raw.github.com/feroc1ty/vukajlija-script/master/vukajlija.js';
             GM_Script.type = 'text/javascript';
             GM_Script.async = true;
             GM_Head.insertBefore(GM_Script, GM_Head.firstChild);
