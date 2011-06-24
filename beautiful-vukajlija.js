481fd27cbc8f75ab3f297ddafeaac987a79d3ab5
Goran Gajic <fero.null@gmail.com>
2011-06-25 00:15:51 +0200

    b

--- beautiful-vukajlija.js
+++ beautiful-vukajlija.js
@@ -89,7 +89,6 @@ var loader = {
 				html += $("<div>").append(this).html();
 			});
 			//self.$paginations.html($res.find(".pagination-container:first").html());
-			self.$container.append(html);
 			$html = $(html);
 			//console.log($html);
 			/*$html.imagesLoaded(function(){
